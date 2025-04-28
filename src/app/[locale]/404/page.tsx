56ac7c71ab33ee3cbdc0fa1f64e623982e5b188e
ahmedeid533 <ahmedeidgoma333@gmail.com>
2025-04-28 12:16:52 +0300

    test cars v0.0012

--- src/app/[locale]/404/page.tsx
+++ src/app/[locale]/404/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from "next";
-import { useLocale } from "next-intl";
 import Image from "next/image";
 
 export const metadata: Metadata = {
@@ -8,7 +7,12 @@ export const metadata: Metadata = {
 };
 
 const page = () => {
-	const locale = useLocale();
+	let locale = "";
+	if (document.location.href.includes("en")) {
+		locale = "en";
+	} else {
+		locale = "ar";
+	}
 	return (
 		<aside className="flex md:flex-col flex-col-reverse justify-center items-center gap-5 custom-container2 py-10">
 			<div className="relative w-3/4 md:w-full h-[60vh]">
