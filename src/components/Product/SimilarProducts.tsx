09a11a97f349d95e3d00534634f96aa1fce14daa
ahmedeid533 <ahmedeidgoma333@gmail.com>
2025-06-18 09:38:56 +0300

    next project

--- src/components/Product/SimilarProducts.tsx
+++ src/components/Product/SimilarProducts.tsx
@@ -21,7 +21,7 @@ const SimilarProducts: React.FC<SimilarProductsProps> = ({ id }) => {
 				const items_ = d.map((item: any) => {
 					return {
 						id: item.id,
-						image: `https://app.3arbitk.com/storage/${item.main_photo}`,
+						image: `${process.env.NEXT_PUBLIC_API}/storage/${item.main_photo}`,
 						title: locale == "en" ? item.name_en : item.name_ar,
 					};
 				});
